1a8b88141f23384b36e64a885e37db712cfc2fc9
IamGeniusORG <tanmayisgenius@gmail.com>
2026-09-05 21:59:00 +0530

    chore: clean up pdf scripts

--- fix-pdf-ts.js
+++
@@ -1,6 +0,0 @@
-﻿const fs = require('fs');
-let content = fs.readFileSync('components/DownloadPdfButton.tsx', 'utf8');
-
-content = content.replace('const opt = {', 'const opt: any = {');
-
-fs.writeFileSync('components/DownloadPdfButton.tsx', content, 'utf8');
